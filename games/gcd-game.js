b4c4b308ab7d6157c0aed93589cda38602203a3a
FearosD <fearos@valar.ru>
2021-03-26 16:01:04 +0300

    add gcd-games

---
+++ games/gcd-game.js
@@ -0,0 +1,29 @@
+import gameController from '../src/index.js';
+
+const gameQuestion = () => {
+  const randNum = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+  const firstNum = randNum(1, 100);
+  const secondNum = randNum(1, 100);
+  const numbers = `${firstNum} ${secondNum}`;
+  return numbers;
+};
+
+const setCorrectAnswer = (question) => {
+  let maxDivisor = 0;
+  const [firstNum, secondNum] = question.split(' ');
+  for (let divisor = 1; divisor <= firstNum; divisor += 1) {
+    const flag = firstNum % divisor === 0 && secondNum % divisor === 0;
+    if (flag) {
+      maxDivisor = maxDivisor > divisor ? maxDivisor : divisor;
+    }
+  }
+  return maxDivisor.toString();
+};
+
+const gameStart = () => {
+  const gamesRules = 'Find the greatest common divisor of given numbers.';
+  const gameData = [gamesRules, gameQuestion, setCorrectAnswer];
+  gameController(gameData);
+};
+
+export default gameStart;
