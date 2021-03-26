13b0f2ff7aaef84d45f319a2412b46fcd805cd8e
FearosD <fearos@valar.ru>
2021-03-26 15:33:30 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,24 +2,29 @@ import readlineSync from 'readline-sync';
 
 const gameController = (gameData) => {
   const [gameRules, question, getCorrectAnswer] = gameData;
+
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your Name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameRules);
+
   for (let round = 1; round <= 3; round += 1) {
     const roundQuestion = question();
     console.log(`Question: ${roundQuestion}`);
+
     const userAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = getCorrectAnswer(roundQuestion);
+
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return;
     }
-    if (round === 3) console.log(`Congratulations, ${userName}!`);
   }
+
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default gameController;
